94caaa41a1112d0940c929d721e96575143d2e27
Jessica-Hasford <jessicahasford18@gmail.com>
2023-05-18 12:27:48 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -13,4 +14,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
+
 #endif
